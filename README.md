4ed4d22005fda41b888f84570e5233f4b1fcb18f
tblackmorepages <tblackmore27@gmail.com>
2022-08-15 16:19:02 +0100

    Version 1 commit

--- README.md
+++ README.md
@@ -3,13 +3,57 @@
 A command line program that accepts an integer number of sides of the die as an input and generates a roll value.
 
 ## Installation
+First download requirements.txt and install in terminal using:
 
+```bash
+pip install -r requirements.txt
+```
+
+Next download the `msdie.py` file. The program can then be run in the terminal using:
+```bash
+python3 msdie.py
+```
 
 ## Usage
+To roll a die of n sides enter the desired value of n. 
+```python
+> 5
+```
 
+Input Integer number of sides must be expressed in numerical or alphabetical format but the latter format must be separated with a single space.
+```python
+> sixty eight 
+```
+
+To re-roll the previously given die enter 'roll' or 'r'.
+```python
+> roll
+```
+
+To roll a die of n sides x number of times enter 'multi roll' or 'm r' followed by n then x.The result will be given as a dictionary of the occurrences of each side of the die.
+```python
+> m r 
+> 5 
+> 10
+```
+
+To exit the calculator: `exit`, `quit` or `close`.'
+
+For help whilst using the calculator enter `help` or `?`. 
 
 ## Example
 
-
-## Unit Testing
-
+```python
+$ python3 msdie.py
+Welcome to MSDie, for help enter: `help` or `?`
+> 3
+2
+> roll
+2
+> m r
+    > 5
+    > 4
+{1: 1, 2: 1, 3: 1, 4: 0, 5: 1}
+> quit
+Exiting MSDie...
+```
--- msdie.py
+++ msdie.py
@@ -20,46 +20,45 @@ class MSDie:
         self.current_value = random.randrange(1, self.num_sides+1)
         return self.current_value
 
+    def __str__(self):
+        return str(self.current_value)
+
+    def __repr__(self):
+        return "MSDie({}) : {}".format(self.num_sides, self.current_value)
+
     def multi_roll(self, rolls):
-        values = []
+        roll_values = {i: 0 for i in range(1, self.num_sides+1)}
         for _ in range(rolls):
-            values.append(self.roll())
-        return values
+            roll_values[self.roll()] += 1
+        return roll_values
 
 
-    def text_to_numbers(self, string):
-        """
-        Converts input text into numbers.
-        """
-        result = []
-        # Split string at each text number
-        str_split = string.split('  ')
-        # If no text numbers return original string and not a single number
-        if len(str_split) == 1:
-            try:
-                w2n.word_to_num(str_split)
-            except:
-                return string
-        # Replace text numbers with digits
+def text_to_numbers(string):
+    """
+    Converts input text into numbers.
+    """
+    result = []
+    # Split string at each text digit
+    str_split = string.split('  ')
+    for sub_str in str_split:
+        word_char_search = re.search('[^a-zA-Z ]', sub_str)
+        if word_char_search is None:
+            number = str(w2n.word_to_num(sub_str))
+            result.append(number)
         else:
-            for sub_str in str_split:
-                word_char_search = re.search('[^a-zA-Z ]', sub_str)
-                if word_char_search is None:
-                    number = str(w2n.word_to_num(sub_str))
-                    result.append(number)
-                else:
-                    result.append(sub_str)
-        return ' '.join(result)
+            result.append(sub_str)
+    return ' '.join(result)
 
 
 def help_info():
-    print('Need to rewrite - RPN calculator usage:\nInput: Expression in Reverse Polish Notation containing whole '
-          'numbers (0-999,999,999,999) and operators separated by a single space. The following '
-          'operators are supported: addition (`+`), subtraction (`-`), multiplication (`*`), integer'
-          ' division (`/`), and modulo (`%`).\ne.g. > 6 11 +\nNumbers can be expressed in numerical '
-          'or alphabetical format but the latter format must be separated with two spaces.\ne.g. > '
-          'six  eleven  +\nOutput: Whole number solution to expression.\nTo exit the calculator: '
-          '`exit`, `quit` or `close`.')
+    print('MSDie Usage:\nTo roll a die of n sides enter the desired value of n.\ne.g. > 5\n'
+          'To re-roll the previously given die enter \'roll\' or \'r\'.\ne.g. > roll\nTo roll '
+          'a die of n sides x number of times enter \'multi roll\' or \'m r\' followed by n '
+          'then x.The result will be given as a dictionary of the occurrences of each side of the '
+          'die.\ne.g. > m r \n> 5 \n> 10. \nInput format: \nInteger number of sides must be '
+          'expressed in numerical or alphabetical format but the latter format must be separated '
+          'with a single space.\ne.g. > sixty eight. \nTo exit the calculator: `exit`, `quit` or '
+          '`close`.')
 
 
 def main():
@@ -76,15 +75,15 @@ def main():
                 print(my_die.roll())
             except:
                 print("Error: please enter number of sides")
-        elif user_input in ["multiple rolls", "rolls", "m rolls", "m r"]:
+        elif user_input in ["multiple rolls", "m r"]:
             die_sides = input("    > ")
             roll_number = input("    > ")
-            my_die = MSDie(int(die_sides))
-            print(my_die.multi_roll(int(roll_number)))
+            my_die = MSDie(int(text_to_numbers(die_sides)))
+            print(my_die.multi_roll(int(text_to_numbers(roll_number))))
         else:
-            # number_input = MSDie.text_to_numbers(user_input)
-            my_die = MSDie(int(user_input))
-            print(my_die.current_value)
+            number_input = text_to_numbers(user_input)
+            my_die = MSDie(int(number_input))
+            print(my_die)
     print("Exiting MSDie...")
 
 
